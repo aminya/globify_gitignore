af685b3bdbd94bc76d87261aad76ebc6c6a55bda
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-10-06 03:46:02 -0500

    test: add unit tests for GlobifyDirectory

--- lib/lib_test.go
+++ lib/lib_test.go
@@ -6,9 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPosixifyPath(t *testing.T) {
+func TestAll(t *testing.T) {
 	assert.Equal(t, PosixifyPath("C:\\hey"), "C:/hey")
 	assert.Equal(t, PosixifyPath("hey/"), "hey/")
+
 	assert.Equal(t, RemoveEndingSlash(PosixifyPath("C:\\hey\\")), "C:/hey")
 	assert.Equal(t, RemoveEndingSlash(PosixifyPath("hey/")), "hey")
+
+	assert.Equal(t, GlobifyDirectory("hey/"), "hey/**")
+	assert.Equal(t, GlobifyDirectory("/home/"), "/home/**")
+	assert.Equal(t, GlobifyDirectory("C:\\hey\\"), "C:/hey/**")
+	assert.Equal(t, GlobifyDirectory("hey\\hey2\\"), "hey/hey2/**")
 }

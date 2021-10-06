cdbe41d33107c3cff91bbdf7d92e667ea3be7d4f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-10-06 03:28:17 -0500

    feat: add PosixifyPathNormalized

--- lib/lib.go
+++ lib/lib.go
@@ -13,3 +13,14 @@ import (
 func PosixifyPath(givenPath string) string {
 	return strings.ReplaceAll(givenPath, "\\", "/")
 }
+
+/**
+ * Converts given path to Posix (replacing \ with /) and removing ending slashes
+ *
+ * @param {string} givenPath Path to convert
+ * @returns {string} Converted filepath
+ */
+func PosixifyPathNormalized(givenPath string) string {
+	return strings.TrimRight(PosixifyPath(givenPath), "/")
+}
+

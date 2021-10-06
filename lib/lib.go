c60ea470bcd4dad35606635e71b8517b9aad2dd2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-10-06 05:17:55 -0500

    feat: add GlobifyGitIgnoreEntry

--- lib/lib.go
+++ lib/lib.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"io/fs"
 	"os"
+	"path"
 	"regexp"
 	"strings"
 )
@@ -144,4 +145,106 @@ func isInvalidPath(path string, extended bool) bool {
 
 func isPath(path string, extended bool) bool {
 	return !isInvalidPath(path, extended)
+}
+
+/**
+ * @param {string} gitIgnoreEntry One git ignore entry (it expects a valid non-comment gitignore entry with no
+ *   surrounding whitespace)
+ * @param {Optional string} gitIgnoreDirectory The directory of gitignore
+ * @returns {[string] | [string, string]} The equivalent glob
+ */
+func GlobifyGitIgnoreEntry(
+	gitIgnoreEntry string,
+	gitIgnoreDirectory ...string,
+) []string {
+	// output glob entry
+	entry := gitIgnoreEntry
+	// Process the entry beginning
+	// '!' in .gitignore means to force include the pattern
+	// remove "!" to allow the processing of the pattern and swap ! in the end of the loop
+	forceInclude := false
+
+	hasGitIgnoreDirectory := len(gitIgnoreDirectory) == 1 // TODO find a better way for optional arguments in Go
+
+	if entry[0] == '!' {
+		entry = entry[1:]
+		forceInclude = true
+	}
+
+	// If there is a separator at the beginning or middle (or both) of the pattern,
+	// then the pattern is relative to the directory level of the particular .gitignore file itself
+	// Process slash
+
+	pathType := PathTypeOther
+
+	if entry[0] == '/' {
+		// Patterns starting with '/' in gitignore are considered relative to the project directory while glob
+		// treats them as relative to the OS root directory.
+		// So we trim the slash to make it relative to project folder from glob perspective.
+		entry = entry[1:]
+
+		// Check if it is a directory or file
+		if isPath(entry, true) {
+			if hasGitIgnoreDirectory {
+				pathType = getPathType(path.Join(gitIgnoreDirectory[0], entry))
+			} else {
+				pathType = getPathType(entry)
+			}
+		}
+	} else {
+		slashPlacement := strings.Index(entry, "/")
+
+		if slashPlacement == -1 {
+			// Patterns that don't have `/` are '**/' from glob perspective (can match at any level)
+			if !strings.HasPrefix(entry, "**/") {
+				entry = "**/" + entry
+			}
+		} else if slashPlacement == len(entry)-1 {
+			// If there is a separator at the end of the pattern then it only matches directories
+			// slash is in the end
+			pathType = PathTypeDirectory
+		} else {
+			// has `/` in the middle so it is a relative path
+			// Check if it is a directory or file
+			if isPath(entry, true) {
+				if hasGitIgnoreDirectory {
+					pathType = getPathType(path.Join(gitIgnoreDirectory[0], entry))
+				} else {
+					pathType = getPathType(entry)
+				}
+			}
+		}
+	}
+
+	// prepend the absolute root directory
+	if hasGitIgnoreDirectory {
+		entry = PosixifyPath(gitIgnoreDirectory[0]) + "/" + entry
+	}
+
+	// swap !
+	if !(forceInclude) {
+		entry = "!" + entry
+	}
+
+	// TODO use a tagged union instead of an array?
+
+	// Process the entry ending
+	if pathType == PathTypeDirectory {
+		// in glob this is equal to `directory/**`
+		if strings.HasSuffix(entry, "/") {
+			return []string{entry + "**"}
+		} else {
+			return []string{entry + "/**"}
+		}
+	} else if pathType == PathTypeFile {
+		// return as is for file
+		return []string{entry}
+	} else if !strings.HasSuffix(entry, "/**") {
+		// the pattern can match both files and directories
+		// so we should include both `entry` and `entry/**`
+		content := entry + "/**"
+		return []string{entry, content}
+	} else {
+		return []string{entry}
+	}
 }
